bba2f2fb757ca89fe3737f856557d130f123970c
wlcn <362747160@qq.com>
2023-09-25 11:18:12 +0800

    add w5-admin-ddd

---
+++ w5-admin-ddd/src/main/java/org/wlcn/w5/admin/W5AdminApplication.java
@@ -0,0 +1,13 @@
+package org.wlcn.w5.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class W5AdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(W5AdminApplication.class, args);
+	}
+
+}
---
+++ w5-admin-ddd/src/test/java/org/wlcn/w5/admin/W5AdminApplicationTests.java
@@ -0,0 +1,14 @@
+package org.wlcn.w5.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class W5AdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+
+}
