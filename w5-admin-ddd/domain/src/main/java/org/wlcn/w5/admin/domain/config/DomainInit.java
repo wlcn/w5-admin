bcabcc9693ddde7eda20b0f550710bf3d8692e28
wlcn <362747160@qq.com>
2023-09-26 22:25:06 +0800

    add WebClient configuration

--- w5-admin-ddd/domain/src/main/java/org/wlcn/w5/admin/domain/config/DomainInit.java
+++ w5-admin-ddd/domain/src/main/java/org/wlcn/w5/admin/domain/config/DomainInit.java
@@ -1,10 +1,13 @@
 package org.wlcn.w5.admin.domain.config;
 
+import com.alibaba.fastjson2.JSONObject;
 import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Component
 @Profile("!test")
@@ -14,9 +17,20 @@ public class DomainInit {
     @Value("${domain.name}")
     private String name;
 
+    @Autowired
+    private WebClient webClient;
+
     @PostConstruct
     public void init() {
         log.info("{} is ready.", name);
+        final var response = webClient.get()
+                .uri("https://cn.bing.com/")
+                .retrieve()
+                .bodyToMono(JSONObject.class);
+
+        response.subscribe(body ->
+                log.info("response body is {}", body)
+        );
     }
 
 }
--- w5-admin-spring-boot-starter/README.md
+++ w5-admin-spring-boot-starter/README.md
@@ -2,9 +2,16 @@
 
 starter for w5 admin
 
+## abilities
+
+1. mybatis-flex
+2. w5 configuration properties
+
 ## usage
 
 ```shell
 # upload to local
 $ ./gradlew.bat publish
+
+$ ./gradlew.bat publishMavenPublicationToMavenLocalRepository
 ```
