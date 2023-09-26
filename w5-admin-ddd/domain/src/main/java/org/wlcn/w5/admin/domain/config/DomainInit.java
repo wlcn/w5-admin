640f3b2146b73f3e1a405f38eaf9f70e00cca8e0
wlcn <362747160@qq.com>
2023-09-26 22:33:12 +0800

    add w5-admin-spring-boot-starter

--- w5-admin-ddd/domain/src/main/java/org/wlcn/w5/admin/domain/config/DomainInit.java
+++ w5-admin-ddd/domain/src/main/java/org/wlcn/w5/admin/domain/config/DomainInit.java
@@ -1,13 +1,10 @@
 package org.wlcn.w5.admin.domain.config;
 
-import com.alibaba.fastjson2.JSONObject;
 import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
-import org.springframework.web.reactive.function.client.WebClient;
 
 @Component
 @Profile("!test")
