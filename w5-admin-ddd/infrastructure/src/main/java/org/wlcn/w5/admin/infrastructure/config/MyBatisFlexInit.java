ca2384238e36ca087e43208bdde1921a5f9f7ae0
wlcn <362747160@qq.com>
2023-09-26 21:52:06 +0800

    refactor w5-admin-ddd with multiple modules

---
+++ w5-admin-ddd/infrastructure/src/main/java/org/wlcn/w5/admin/infrastructure/config/MyBatisFlexInit.java
@@ -0,0 +1,40 @@
+package org.wlcn.w5.admin.infrastructure.config;
+
+import cn.hutool.core.io.FileUtil;
+import cn.hutool.core.io.resource.ResourceUtil;
+import cn.hutool.core.util.StrUtil;
+import com.mybatisflex.core.datasource.DataSourceKey;
+import com.mybatisflex.core.row.Db;
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+import org.wlcn.w5.admin.util.W5Util;
+
+@Component
+@Profile("local")
+@Slf4j
+public class MyBatisFlexInit {
+
+    @PostConstruct
+    public void init() {
+        Db.tx(() -> {
+            try {
+                final var dmsSqlLineList = FileUtil.readUtf8Lines(ResourceUtil.getResource("db/h2-schema.sql"));
+                final var ddlSqlLineList = FileUtil.readUtf8Lines(ResourceUtil.getResource("db/h2-data.sql"));
+                final var dmlSql = StrUtil.join(W5Util.LINE_NEXT, dmsSqlLineList);
+                final var ddlSql = StrUtil.join(W5Util.LINE_NEXT, ddlSqlLineList);
+                DataSourceKey.use(W5Util.DataSourceEnum.DATASOURCE_1.getKey());
+                Db.updateBySql(dmlSql);
+                Db.updateBySql(ddlSql);
+                log.info("INIT SQL at local done");
+            } catch (Exception e) {
+                log.error("INIT sql error.", e);
+            } finally {
+                DataSourceKey.clear();
+            }
+            return Boolean.TRUE;
+        });
+    }
+
+}
