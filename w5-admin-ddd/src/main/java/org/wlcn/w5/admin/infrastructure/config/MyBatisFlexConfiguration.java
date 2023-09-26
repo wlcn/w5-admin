839dde14f56cd203db5d681746338f4135c698ce
wlcn <362747160@qq.com>
2023-09-26 09:23:19 +0800

    update w5-admin-ddd

--- w5-admin-ddd/src/main/java/org/wlcn/w5/admin/infrastructure/config/MyBatisFlexConfiguration.java
+++ w5-admin-ddd/src/main/java/org/wlcn/w5/admin/infrastructure/config/MyBatisFlexConfiguration.java
@@ -1,21 +1,14 @@
 package org.wlcn.w5.admin.infrastructure.config;
 
-import cn.hutool.core.io.FileUtil;
 import com.mybatisflex.core.audit.AuditManager;
-import com.mybatisflex.core.datasource.DataSourceKey;
-import com.mybatisflex.core.row.Db;
 import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
-import org.wlcn.w5.admin.util.W5Util;
-
-import java.nio.charset.Charset;
 
 @Configuration
 @MapperScan("org.wlcn.w5.admin.infrastructure.*.mapper")
 @Slf4j
-public class MyBatisFlexConfiguration implements CommandLineRunner {
+public class MyBatisFlexConfiguration {
 
     public MyBatisFlexConfiguration() {
         //开启审计功能
@@ -28,21 +21,4 @@ public class MyBatisFlexConfiguration implements CommandLineRunner {
         );
     }
 
-    @Override
-    public void run(String... args) throws Exception {
-        Db.tx(() -> {
-            try {
-                final var dmlSql = FileUtil.readString("db/h2-schema.sql", Charset.defaultCharset());
-                final var ddlSql = FileUtil.readString("db/h2-data.sql", Charset.defaultCharset());
-                DataSourceKey.use(W5Util.DataSourceEnum.DATASOURCE_1.getKey());
-                Db.updateBySql(dmlSql);
-                Db.updateBySql(ddlSql);
-            } catch (Exception e) {
-                log.error("INIT sql error.", e);
-            } finally {
-                DataSourceKey.clear();
-            }
-            return Boolean.TRUE;
-        });
-    }
 }
