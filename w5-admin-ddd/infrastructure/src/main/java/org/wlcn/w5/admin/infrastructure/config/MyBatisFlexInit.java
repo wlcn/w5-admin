185cd2c103c81be02dcf6c0a1d6faaee61ac42c7
wlcn <362747160@qq.com>
2023-09-26 12:37:44 +0800

    refactor w5-admin-ddd with multiple modules

--- w5-admin-ddd/infrastructure/src/main/java/org/wlcn/w5/admin/infrastructure/config/MyBatisFlexInit.java
+++ w5-admin-ddd/infrastructure/src/main/java/org/wlcn/w5/admin/infrastructure/config/MyBatisFlexInit.java
@@ -5,11 +5,10 @@ import com.mybatisflex.core.datasource.DataSourceKey;
 import com.mybatisflex.core.row.Db;
 import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 import org.wlcn.w5.admin.util.W5Util;
-
-import java.nio.charset.Charset;
 
 @Component
 @Profile("local")
@@ -20,8 +19,8 @@ public class MyBatisFlexInit {
     public void init() {
         Db.tx(() -> {
             try {
-                final var dmlSql = FileUtil.readString("db/h2-schema.sql", Charset.defaultCharset());
-                final var ddlSql = FileUtil.readString("db/h2-data.sql", Charset.defaultCharset());
+                final var dmlSql = FileUtil.readUtf8String("classpath:db/h2-schema.sql");
+                final var ddlSql = FileUtil.readUtf8String("classpath:db/h2-data.sql");
                 DataSourceKey.use(W5Util.DataSourceEnum.DATASOURCE_1.getKey());
                 Db.updateBySql(dmlSql);
                 Db.updateBySql(ddlSql);
